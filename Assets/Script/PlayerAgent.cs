3a0f6bffe63e0bac3d3ec2edad0e26dcd23eb474
masterxolos <ozgur-2000-6@hotmail.com>
2022-12-15 15:39:00 +0300

    add: player option

--- Assets/Script/PlayerAgent.cs
+++ Assets/Script/PlayerAgent.cs
@@ -13,6 +13,8 @@ public class PlayerAgent : Agent
     private Transform ballTransform;
     [SerializeField]
     private float moveSpeed = 5f;
+
+    [SerializeField] private bool isPlayer;
     
     public override void OnEpisodeBegin() 
     { 
@@ -29,14 +31,30 @@ public class PlayerAgent : Agent
     public override void Heuristic(in ActionBuffers actionsOut)
     {
         ActionSegment<float> continuousActions = actionsOut.ContinuousActions;
-        continuousActions[0] = Input.GetAxisRaw("Vertical");
+        if (Input.touches.Length > 0)
+        {
+            Vector3 touchPosition = Input.touches[0].position;
+            touchPosition = Camera.main.ScreenToWorldPoint(touchPosition);
+            touchPosition.y = Mathf.Clamp(touchPosition.y, -3.10f,3.10f);
+            continuousActions[0] = touchPosition.y;
+        }
     }
     
     public override void OnActionReceived(ActionBuffers actions)
     {
-        float moveY = actions.ContinuousActions[0];
-        transform.localPosition += new Vector3(0, moveY, 0) * Time.deltaTime * moveSpeed;
-        AddReward(-0.0015f);
+        if (isPlayer)
+        { 
+            var newPos = transform.localPosition;
+            newPos.y = actions.ContinuousActions[0];
+            transform.localPosition = newPos; 
+        }
+        else
+        {
+            float moveY = actions.ContinuousActions[0];
+            transform.localPosition += new Vector3(0, moveY, 0) * Time.deltaTime * moveSpeed;
+            AddReward(-0.0015f);
+        }
+        
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
